05f683c4b7ac8aa782d9508631213552ee58b39e
xTwi7ch <jonssonnick@gmail.com>
2014-06-12 21:57:32 +0100

    Bluetooth paired and working

---
+++ testbt.js
@@ -0,0 +1,10 @@
+var five = require("johnny-five");
+
+var board = new five.Board({
+  port: "/dev/tty.LiveLP-DevB"
+});
+
+board.on("ready", function(){
+  //Test Led on pin 13 and strobe it over bluetooth
+  (new five.Led(13)).strobe();
+});
